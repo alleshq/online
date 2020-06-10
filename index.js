e63a04cb11ca994269d559e2d6f66fb5074769c0
Archie Baer <archie@abaer.dev>
2020-06-11 00:46:18 +0100

    404 and user routes

--- index.js
+++ index.js
@@ -5,6 +5,15 @@ app.listen(3000);
 const db = require("./db");
 const auth = require("./auth.js");
 
+// Online
 app.get("/", auth, (req, res) => {
     if (!req.user) return res.status(401).send("🔴");
 });
+
+// User
+app.get("/:user", (req, res) => {
+    
+});
+
+// 404
+app.use((req, res) => res.status(404).send("🔴"));
